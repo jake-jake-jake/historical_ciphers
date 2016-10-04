41418ebc41d5c2b7a65699d2bf26a771ee6f422d
jake-jake-jake <jcdavis@awedge.net>
2016-10-04 13:25:09 -0400

    MANIFEST to gitignore, package > library

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setup(
     name = 'historical_ciphers',
     packages = ['historical_ciphers'], # this must be the same as the name above
     version = '0.1',
-    description = 'Python library for encrypting, decrypting, and hacking historical ciphers.',
+    description = 'Python package for encrypting, decrypting, and hacking historical ciphers.',
     author = 'Jake Davis',
     author_email = 'jcdavis@awedge.net',
     url = 'https://github.com/jake-jake-jake/historical_ciphers',
