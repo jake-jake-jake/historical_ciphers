1271f81f55907488f55e74af8924dd542e51ba6f
jake-jake-jake <jcdavis@awedge.net>
2016-10-04 13:00:58 -0400

    Packaging for PyPI

---
+++ setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+
+setup(
+    name = 'historical_ciphers',
+    packages = ['historical_ciphers'], # this must be the same as the name above
+    version = '0.1',
+    description = 'Python library for encrypting, decrypting, and hacking historical ciphers.',
+    author = 'Jake Davis',
+    author_email = 'jcdavis@awedge.net',
+    url = 'https://github.com/jake-jake-jake/historical_ciphers',
+    download_url = 'https://github.com/jake-jake-jake/historical_ciphers/tarball/0.1',
+    keywords = ['ciphers', 'cryptography', 'history'], # arbitrary keywords
+    classifiers = [],
+)
